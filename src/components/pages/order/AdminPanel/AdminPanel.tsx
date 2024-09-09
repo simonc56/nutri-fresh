f4ab0fac49dd46c46ab59fc49446a1ec26de4a1f
simonc56 <simchat@gmail.com>
2024-09-09 21:21:11 +0200

    🚧 F07: ORDER > add AdminPanel

---
+++ src/components/pages/order/AdminPanel/AdminPanel.tsx
@@ -0,0 +1,5 @@
+import "./AdminPanel.scss";
+
+export default function AdminPanel() {
+  return <section className="admin-panel">AdminPanel</section>;
+}
--- src/components/pages/order/OrderPage.tsx
+++ src/components/pages/order/OrderPage.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-
+import AdminPanel from "./AdminPanel/AdminPanel";
 import Main from "./Main/Main";
 import Navbar from "./Navbar/Navbar";
 import "./OrderPage.scss";
@@ -12,6 +12,7 @@ function OrderPage() {
       <div className="container">
         <Navbar username={username || ""} />
         <Main />
+        <AdminPanel />
       </div>
     </div>
   );
