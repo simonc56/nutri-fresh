898ede9633723d24f1f03c2a3fba272c040b8414
simonc56 <simchat@gmail.com>
2024-09-11 18:47:58 +0200

    🐛 F08: ORDER > improve panels layout with grid

--- src/components/pages/order/Main/Main.tsx
+++ src/components/pages/order/Main/Main.tsx
@@ -7,7 +7,7 @@ export default function Main() {
   const { isAdminMode } = useOrderContext();
   return (
     <main className="main">
-      <section className="basket">Basket</section>
+      {/* <section className="basket">Basket</section> */}
       <section className="menu-and-admin">
         <Menu />
         {isAdminMode && <AdminPanel />}
