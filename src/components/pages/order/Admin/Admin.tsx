0059ebb4ff3aecca1f413412b677c2b8d7b78373
simonc56 <simchat@gmail.com>
2024-10-29 14:20:04 +0100

    🚧 F14: Admin > open with slide animation

--- src/components/pages/order/Admin/Admin.tsx
+++ src/components/pages/order/Admin/Admin.tsx
@@ -1,15 +1,20 @@
+import { useRef } from "react";
+import { CSSTransition } from "react-transition-group";
 import { useOrderContext } from "src/context/useOrderContext";
 import "./Admin.scss";
 import AdminPanel from "./AdminPanel/AdminPanel";
 import AdminTabs from "./AdminTabs/AdminTabs";
 
 export default function Admin() {
-  const { isPanelOpen } = useOrderContext();
+  const { isPanelOpen, isAdminMode } = useOrderContext();
+  const nodeRef = useRef(null);
 
   return (
-    <div className="admin">
-      <AdminTabs />
-      {isPanelOpen && <AdminPanel />}
-    </div>
+    <CSSTransition nodeRef={nodeRef} in={isAdminMode} timeout={300} classNames="admin-transition">
+      <div ref={nodeRef} className="admin">
+        <AdminTabs />
+        {isPanelOpen && <AdminPanel />}
+      </div>
+    </CSSTransition>
   );
 }
